27e85bb942f93c8cfcbed44da87039b51b4af049
Michael Nascimento <michael.nsc@outlook.com>
2022-08-18 22:33:18 -0300

    Ex 5

--- src/components/ListaCards/ListaCards.jsx
+++ src/components/ListaCards/ListaCards.jsx
@@ -3,20 +3,25 @@ import PropTypes from "prop-types";
 import { Card } from "@components";
 
 import styles from "./ListaCards.module.css";
+import { useState } from "react";
 
-export const ListaCards = ({ produtos }) => (
-  <ul className={styles.lista}>
-    {produtos.map((produto) => (
-      <li className={styles.listaItem} key={produto.id}>
-        <Card
-          produto={produto}
-          selecionado={false}
-          onSelecionar={() => console.log("Clicou no card!")}
-        />
-      </li>
-    ))}
-  </ul>
-);
+export const ListaCards = ({ produtos }) => {
+  const [idSelecionados, setIdSelecionados] = useState([]);
+
+  return (
+    <ul className={styles.lista}>
+      {produtos.map((produto) => (
+        <li className={styles.listaItem} key={produto.id}>
+          <Card
+            produto={produto}
+            selecionado={false}
+            onSelecionar={() => console.log("Clicou no card!")}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 ListaCards.propTypes = {
   produtos: PropTypes.array.isRequired,

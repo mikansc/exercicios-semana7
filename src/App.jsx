a9a4974a266c779a6c1531c2514f05c6ebf11f5d
Michael Nascimento <michael.nsc@outlook.com>
2022-08-24 08:25:12 -0300

    ex 8

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,31 @@ import { Footer, Header, Secao } from "@components";
 import produtos from "@services/produtos.json";
 import styles from "./App.module.css";
 
+const subSecoesEntradas = new Set(produtos.entradas.map((p) => p.subSecao));
+const subSecoesPrincipais = new Set(produtos.principais.map((p) => p.subSecao));
+
+const secoes = [
+  { nome: "Entradas", produtos: produtos.entradas, subSecoes: subSecoesEntradas },
+  { nome: "Principais", produtos: produtos.principais, subSecoes: subSecoesPrincipais },
+  { nome: "Sobremesas", produtos: produtos.sobremesas },
+];
+
 function App() {
-  const subSecoesEntradas = new Set(produtos.entradas.map((p) => p.subSecao));
-  const subSecoesPrincipais = new Set(produtos.principais.map((p) => p.subSecao));
   console.log(subSecoesEntradas, subSecoesPrincipais);
 
   return (
     <div className={styles.app}>
       <Header />
       <main className={styles.main}>
-        <Secao
-          nome="Entradas"
-          produtos={produtos.entradas}
-          subSecoes={Array.from(subSecoesEntradas)}
-        />
-
-        <Secao
-          nome="Principais"
-          produtos={produtos.principais}
-          subSecoes={Array.from(subSecoesPrincipais)}
-        />
+        {secoes.map((secao) => {
+          return (
+            <Secao
+              nome={secao.nome}
+              produtos={secao.produtos}
+              subSecoes={secao.subSecoes ? Array.from(secao.subSecoes) : null}
+            />
+          );
+        })}
 
         <Secao nome="Sobremesas" produtos={produtos.sobremesas} />
       </main>
